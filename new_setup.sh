8108cd2ccaf98bd87376bd9d50268668a9f29f81
Shaurya K. Panwar <63470280+KlrShaK@users.noreply.github.com>
2024-04-30 15:57:16 +0530

    Update new_setup.sh

--- new_setup.sh
+++ new_setup.sh
@@ -4,18 +4,18 @@ set -o pipefail
 
 sudo -v
 echo "Starting Install!" 
-sudo apt install git
+sudo apt -y install git
 echo "Installed Git!" 
-sudo apt install xpad
+sudo apt -y install xpad
 echo "Installed xpad!" 
-sudo apt install python3-pip
+sudo apt -y install python3-pip
 echo "Installing python3-pip"
-sudo apt install python3-virtualenv
+sudo apt -y install python3-virtualenv
 echo "Installed virtualenv!" 
 
 mkdir ~/work/
 
-sudo apt install vim
+sudo apt -y install vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp .vimrc ~/.
 echo "Installed Vim!" 
@@ -25,11 +25,11 @@ wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/
 sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/3bf863cc.pub
 sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
-sudo apt-get update
-sudo apt-get install cuda-11-7
+sudo apt-get -y update
+sudo apt-get -y install cuda-11-7
 # --------------------------------------------------------------------
 echo "Installed CUDA!" 
 
 sudo add-apt-repository ppa:flexiondotorg/nvtop
-sudo apt install nvtop
+sudo apt -y install nvtop
 echo "Installed NVTOP!" 
